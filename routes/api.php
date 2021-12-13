6ee3049a969bb553e15c9c28949e2179c169f60e
Arnold <arnold201991@gmail.com>
2021-12-13 19:02:15 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsuarioController;
 use App\Http\Controllers\PartyController;
+use App\Http\Controllers\JuegoController;
+use App\Http\Controllers\MensajeController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
